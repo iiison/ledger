efffd4c261b97f2e809469fad3c9f1e4fabcfbd4
iiison <i.bharat.soni@gmail.com>
2024-12-10 11:09:41 +0530

    Dont Know o

--- packages/api/src/common/models/serviceResponse.ts
+++ packages/api/src/common/models/serviceResponse.ts
@@ -1,5 +1,5 @@
-import { z } from 'zod'
-import { StatusCodes } from 'http-status-codes'
+import { z } from "zod";
+import { StatusCodes } from "http-status-codes";
 
 export enum ResponseStatus {
   Success,
@@ -16,31 +16,33 @@ export class ServiceResponse<T = null> {
     status: ResponseStatus,
     message: string,
     responseObject: T,
-    statusCode: number
+    statusCode: number,
   ) {
-    this.success = status === ResponseStatus.Success
-    this.message = message
-    this.responseObject = responseObject
-    this.statusCode = statusCode
+    this.success = status === ResponseStatus.Success;
+    this.message = message;
+    this.responseObject = responseObject;
+    this.statusCode = statusCode;
   }
 }
 
-export const ServiceResponseSchema = <T extends z.ZodTypeAny>(dataSchema: T) => {
+export const ServiceResponseSchema = <T extends z.ZodTypeAny>(
+  dataSchema: T,
+) => {
   return z.object({
     success: z.boolean(),
     message: z.string(),
     responseObject: dataSchema.optional(),
-    statusCode: z.number()
-  })
-}
+    statusCode: z.number(),
+  });
+};
 
 export function buildNotFoundServiceResponse(message: string) {
   return new ServiceResponse(
     ResponseStatus.Failed,
     message,
     null,
-    StatusCodes.NOT_FOUND
-  )
+    StatusCodes.NOT_FOUND,
+  );
 }
 
 export function buildInternalErrorServiceResponse(message: string) {
@@ -48,8 +50,8 @@ export function buildInternalErrorServiceResponse(message: string) {
     ResponseStatus.Failed,
     message,
     null,
-    StatusCodes.NOT_FOUND
-  )
+    StatusCodes.NOT_FOUND,
+  );
 }
 
 export function buildBadReqServiceResponse(message: string) {
@@ -57,18 +59,15 @@ export function buildBadReqServiceResponse(message: string) {
     ResponseStatus.Failed,
     message,
     null,
-    StatusCodes.BAD_REQUEST
-  )
+    StatusCodes.BAD_REQUEST,
+  );
 }
 
-export function buildSuccessServiceResponse<T>(
-  message: string,
-  data: T
-) {
+export function buildSuccessServiceResponse<T>(message: string, data: T) {
   return new ServiceResponse(
     ResponseStatus.Success,
     message,
     data,
-    StatusCodes.OK
-  )
+    StatusCodes.OK,
+  );
 }
